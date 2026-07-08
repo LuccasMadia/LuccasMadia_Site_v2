5bb28e88ab88a2db499267d0b0d9e21f3776c305
LuccasMadia <root@DESKTOP-46FLVVK.localdomain>
2026-07-08 14:59:49 -0400

    chore: scaffold Vite + React + TS + Tailwind com fonte Kanit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="bg-[#0C0C0C] font-kanit" style={{ overflowX: 'clip' }}>
+      <h1 className="hero-heading p-10 text-6xl font-black uppercase">Madia</h1>
+    </main>
+  )
+}
